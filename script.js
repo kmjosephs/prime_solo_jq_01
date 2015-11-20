6289f8441dade6bf567752e34c22da7fa03b0bf9
Keisha Josephs <kmjosephs@Keishas-MacBook-Pro.local>
2015-11-20 11:01:46 -0600

    Got form working and printing to screen

--- script.js
+++ script.js
@@ -1,73 +1,32 @@
 $(function(){
-
-
-  function Employee(firstName, lastName, employeeNumber, position, score, salary){
-      this.firstName = firstName;
-      this.lastName = lastName;
-      this.employeeNumber = employeeNumber;
-      this.position = position;
-      this.score = score;
-      this.salary = salary;
-    }
-
-  var employeeArray = []; // creates an array to store the objects
-
-  $('form#employeeInfo').on('submit', function(event){
-
-    var employeeData = $(this).serializeArray(); //this pushes the form data to the array on submit
-    for (var i=0; i < employeeData.length; i++){
-      employeeArray.push(employeeData[i]); //creates an array of objects in employeeArray
-    }
-    event.preventDefault();
-    console.log(employeeArray); // Just so I can tell that the code doing something
-  })
-/* I need to create a new Employee object with the elements in the array, could I use a forEach();
-  employeeArray.forEach(function(elem){
-  }) */
-
-var newEmployee =  new Employee(employeeArray[0].value, employeeArray[1].value, employeeArray[2].value, employeeArray[3].value, employeeArray[4].value, employeeArray[5].value); //Maybe create a new object with the elemnts from the employee array
-// returns "TypeErro:employeeArray[0] is undefined"
-
-console.log("This is a test: ")
-console.log(newEmployee.score);
 /**
-=======================
-This code removes data from an element
-$( "button" ).click(function() {
-  $( "p" ).empty();
-});
-
-=======================
+*
 */
-    /*
-
-
+  function Employee(firstName, lastName, employeeNumber, position, score, salary){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.employeeNumber = employeeNumber;
+        this.position = position;
+        this.score = score;
+        this.salary = salary;
       }
 
-      for
+    $('#employeeInfo').on('submit', function(event){
 
-      function writeToScreen{
-        var $div = $('<div>');
-        var $p = $('<p>');
-        var $button = $('<button>');
-        var $ul = $('<ul>');
-        var $li = $('<li>');
+      var employeeData = $(this).serializeArray();
+      console.log(employeeData);
 
-        $ul.append()...
+      for (var i = 0; i < employeeData.length-1; i++){
+        var employee = new Employee(employeeData[0].value, employeeData[1].value,
+          employeeData[2].value, employeeData[3].value, employeeData[4].value);
+
       }
+      $('.placedContent').append('<p>' + employee.firstName + ' ' + employee.lastName
+       + ' ' + employee.employeeNumber + ' ' + employee.position + '</p>')
 
-
-      function writeToScreen(elem){
-        firstName = elem[0].value;
-
-        var newEmployee = new Employee(firstName)
-        return employee;
-      }
-========================
-    var employeeData = $(this).serializeArray();
-    console.log(employeeData); */
-
-
+      console.log(employee);
+      event.preventDefault();
+    });
 
 
 
