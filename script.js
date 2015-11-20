71dda6eb355ea3b1bc68b7731998eae07c2706c4
Keisha Josephs <kmjosephs@Keishas-MacBook-Pro.local>
2015-11-20 13:18:11 -0600

    Got handlebars to print some info to page

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(function(){
 /**
-*
+* Builds the Employee constructor to take in employee information
 */
   function Employee(firstName, lastName, employeeNumber, position, score, salary){
         this.firstName = firstName;
@@ -11,7 +11,13 @@ $(function(){
         this.salary = salary;
       }
 
+
+/**
+* Grabs the information from the form on submit, breaks it into an array of objects
+then constructs a new employee object with the values from the array of objects
+*/
     $('#employeeInfo').on('submit', function(event){
+
 
       var employeeData = $(this).serializeArray();
       console.log(employeeData);
@@ -19,14 +25,19 @@ $(function(){
       for (var i = 0; i < employeeData.length-1; i++){
         var employee = new Employee(employeeData[0].value, employeeData[1].value,
           employeeData[2].value, employeeData[3].value, employeeData[4].value);
-
       }
-      $('.placedContent').append('<p>' + employee.firstName + ' ' + employee.lastName
-       + ' ' + employee.employeeNumber + ' ' + employee.position + '</p>')
 
-      console.log(employee);
+      var templateScript = $("#employeeListScript").html();
+      var makeTemplate = Handlebars.compile(templateScript);
+      var compiledHtml = makeTemplate({employees:employeeData});
+      $('.employeeList').html(compiledHtml);
+      /*$('.placedContent').append('<p>' + employee.firstName + ' ' + employee.lastName
+       + ' ' + employee.employeeNumber + ' ' + employee.position + '</p>')*/
+
       event.preventDefault();
     });
+
+
 
 
 
